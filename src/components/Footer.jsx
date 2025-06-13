23055e605375f33978589ecfc810b334d559d735
Ayesha <151128285+AyeshaShahid03@users.noreply.github.com>
2025-06-13 22:44:36 +0500

    paths updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaInstagram, FaXTwitter, FaLinkedin, FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/ayeshaa24_?igsh=MW5yenI5bjJxZDNpcg%3D%3D&utm_source=qr"
+        href="https://www.instagram.com/saaloashh?igsh=MW5yenI5bjJxZDNpcg%3D%3D&utm_source=qr"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
+      </a>
+      <a
+        href="https://github.com/AyeshaShahid03"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub size={24} />
       </a>
       {/* <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
         <FaXTwitter size={24} />
